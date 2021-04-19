341f96ebfd4c781d746f47402d82f8a1998a98ff
Prabhashankar Kannapan <shankar75031@gmail.com>
2021-04-20 00:28:22 +0530

    added user component and scoped styling

--- pages/auth/index.jsx
+++ pages/auth/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import User from "../../components/User";
 
 export default function authIndexPage() {
   return (
     <div>
       <h1>The auth page</h1>
+      <User name="Kunj" age={28} />
     </div>
   );
 }
